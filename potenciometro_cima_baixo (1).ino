0e939392ce7722f2cb3d2f041dd18229bf3fa2e7
YuriLG-senai <yrui8769@gmail.com>
2024-10-09 19:27:20 -0300

    Add files via upload

---
+++ potenciometro_cima_baixo (1).ino
@@ -0,0 +1,26 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int xValue = analogRead(A0);
+  int yValue = analogRead(A1);
+
+  if (yValue < 341) {
+    Serial.println("Para cima");
+  } else if (yValue > 682) {
+    Serial.println("Para baixo");
+  } else {
+    Serial.println("Centralizado");
+  }
+
+  if (xValue < 341) {
+    Serial.println("Para esquerda");
+  } else if (xValue > 682) {
+    Serial.println("Para direita");
+  } else {
+    Serial.println("Centralizado");
+  }
+
+  delay(100);
+}
